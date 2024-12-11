acb9242d29b628c0ac73a6fe652c1827180f4c8b
lexuanbinh <lb@silverliningresearch.com>
2024-12-11 15:08:01 +0700

    update files

--- scripts/data_search.js
+++ scripts/data_search.js
@@ -11,6 +11,9 @@ function parse_location_info(question, value) {
   api.fn.answers({Q0B:  location_info[1]});  //Overnatningssted - accomodation name
   api.fn.answers({Q0D: Number(location_info[2])});               //accomodation type
   
+  api.fn.answers({destination_name: (location_info[6])});               //accomodation type
+  api.fn.answers({destination_size: (location_info[7])});               //accomodation type
+
   console.log("location_info[4]:", location_info[4]); 
 
   //Q0A_area
